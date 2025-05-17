ba83234e9129ded305dd94378796e956fd1e7daa
kosciolek <noemail>
2025-05-18 00:18:41 +0200

    init

---
+++ index.ts
@@ -0,0 +1,56 @@
+import puppeteer from 'puppeteer';
+import { mkdir } from 'node:fs/promises';
+import { existsSync } from 'node:fs';
+import path from 'node:path';
+const browser = await puppeteer.launch({
+    headless: false,
+    defaultViewport: {
+        width: 1080,
+        height: 1024
+    }
+});
+
+
+const DOWNLOAD_PATH = path.resolve(import.meta.dirname, 'clonk-downloads');
+
+if (!existsSync(DOWNLOAD_PATH)) {
+    await mkdir(DOWNLOAD_PATH);
+}
+
+
+const page = await browser.newPage();
+const client = await page.createCDPSession()
+await client.send('Page.setDownloadBehavior', {
+  behavior: 'allow',
+  downloadPath: DOWNLOAD_PATH,
+})
+
+
+// currently there are around 120 pages of results, so lets do 150 just to be sure
+const EXPECTED_PAGES = 150;
+for (let p = 0; p < EXPECTED_PAGES; p++) {
+    console.log(`Processing page ${p} of ${EXPECTED_PAGES}`);
+    await page.goto(`https://ccan.de/cgi-bin/ccan/ccan-view.pl?a=&ac=ty-ti-ni-tm-vo&nr=30&sc=vo&so=d&pg=${p}`)
+    const links = await page.$$("table tbody tr:not(:first-child) td:nth-child(3) a")
+    
+      const ACCEPTABLE_LINK_REGEX = /\.c4d$|\.zip$/;
+
+      for (const link of links) {
+        const linkData = await link.evaluate(el => ({
+            href: el.href,
+            textContent: el.textContent
+        }));
+        
+        if (ACCEPTABLE_LINK_REGEX.test(linkData.href)) {
+          await link.click();
+          console.log(`Downloading ${linkData.textContent}`);
+          await new Promise((r) => setTimeout(r, 1000));
+        }
+      }
+      
+}
+
+
+
+
+await browser.close();
